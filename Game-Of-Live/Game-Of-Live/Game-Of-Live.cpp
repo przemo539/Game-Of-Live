9dd61ab65a92e852cdcfa664e64d8658101fefaa
przemo5391@gmail.com <przemo5391@gmail.com>
2019-03-21 11:05:10 +0100

    Added a boost file config and boundary condition. In setting.ini introduced glider and glider Gun

--- Game-Of-Live/Game-Of-Live/Game-Of-Live.cpp
+++ Game-Of-Live/Game-Of-Live/Game-Of-Live.cpp
@@ -1,26 +1,81 @@
-﻿// GraWZycie.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
-#include "pch.h"
+﻿#include "pch.h"
 #include "engine.h"
 #include <iostream>
+#include <boost/program_options.hpp>
+#include <fstream>
+#include <string>
+#include <thread>
 
+
+namespace po = boost::program_options;
+
+void read_settings(po::options_description& fileOptions,
+	po::variables_map& vm)
+{
+	std::ifstream settings_file("settings.ini");
+
+	// Clear the map.
+	vm = po::variables_map();
+	if(settings_file.is_open()) {
+		po::store(po::parse_config_file(settings_file, fileOptions), vm);
+		po::notify(vm);
+	} else {
+		cout << "We have some trouble here.... We got error in parsing config file.." << endl << "App wil be closing";
+	}
+}
+
+void write_settings(const char* name, const char* value)
+{
+	std::ofstream settings_file("settings.ini");
+	if (settings_file.is_open()) {
+		settings_file << name << " = " << value;
+	}
+}
+
+void call(engine *game) {
+	while (1) {
+		game->move();
+		std::this_thread::sleep_for(std::chrono::milliseconds(25));
+	}
+}
 
 int main()
 {
-	system("cls");
-	engine game;
-	game.init(10);
-	game.start();
+	int size = 0;
+	vector<std::string> glider;
+	vector<std::string> gliderGun;
+	try
+	{
+		po::options_description fileOptions{ "File" };
+		fileOptions.add_options()
+			("glider", po::value<vector<std::string>>(&glider)->multitoken()->composing(), "Struct of Glider on grid")
+			("gliderGun", po::value<vector<std::string>>(&gliderGun)->multitoken()->composing(), "Struct of GliderGun on grid")
+			("size", po::value<int>(&size), "Size of grid");
+		po::variables_map vm;
+
+
+		read_settings(fileOptions, vm);
+	}
+	catch (const po::error &ex)
+	{
+		std::cerr << ex.what() << '\n' << "app will be closing" << endl;
+	}
+	if (size > 0) {
+		int menu = 0;
+		if (!glider.empty() || !gliderGun.empty()) {			
+			cout << "Starting menu: "  << endl << (!glider.empty() ? "1. load Glider" : " ") << endl << (!gliderGun.empty() ? "2. load Gosper glider gun" : "") << endl << "Type number: ";
+			cin.sync();
+			cin >> menu;
+
+		}
+	
+		system("cls");
+		
+		engine game;
+		thread th(call, &game);
+		th.joinable();
+		game.init(size, ((menu == 1) ? glider : ((menu == 2) ? gliderGun : (vector<string>)(NULL))));
+		game.start();
+		th.join();
+	}
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
--- Game-Of-Live/Game-Of-Live/engine.h
+++ Game-Of-Live/Game-Of-Live/engine.h
@@ -1,18 +1,27 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include <thread>
 using namespace std;
+
+
 class engine
 {
+	friend void tick(engine);
 	vector<vector<bool>> grid;
 	int generation;
 	int size;
 	bool ready2start;
+	bool periodMakeMover;
 	void newGen();
 	void render();
 	void menu();
+	void string2grid(vector <string>);
+	void makeHalfSecsMove();
+	void tick();
 public:
-	void init(int size);
+	void move();
+	void init(int size, vector <string>);
 	void start();
 	engine();
 	~engine();
